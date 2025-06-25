51ded34131c9e7195b8683d4989183c5ed1d220d
Irving Salazar <ISCIrvingDev@gmail.com>
2025-06-24 23:51:07 -0600

    Clean Project

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { Image } from 'expo-image';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+        />
+      }>
+      <ThemedView>
+        <ThemedText type="title">Welcome!</ThemedText>
+        <HelloWave />
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
